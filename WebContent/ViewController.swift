f6a36c720b781db98183f32b6dde04ea971ec750
MagmMe <marcin@magm.me>
2020-10-03 12:25:06 +0200

    Initial Commit

---
+++ WebContent/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WebContent
+//
+//  Created by Marcin Magiera on 03/10/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
